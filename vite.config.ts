3d7ac2733b8871be9eadf7ebd75381fbc03a3358
Kenjy Jap <kenhiebusiness@gmail.com>
2024-11-17 10:49:43 -0800

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from "dotenv";
 dotenv.config({ path: "./environment/.env" });
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './enviornment',
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects", // Path to your _redirects file
+          dest: ".", // Copy it to the root of the dist folder
+        },
+      ],
+    })
+  ],
   base: "/", // Base URL for assets
   build: {
     outDir: "dist", // Output directory
